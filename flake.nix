f89337cc35682c7855be15b00b91acf062da8020
ghetto <ghetto.ch@gmail.com>
2026-05-09 15:02:55 +0200

    remove wsl entirelyremove wsl entirely

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, ... }:
@@ -15,25 +14,6 @@
       home.enableNixpkgsReleaseCheck = false;
 
       nixosConfigurations = {
-        # nixos-wsl = nixpkgs.lib.nixosSystem {
-        #   system = "x86_64-linux";
-        #   specialArgs = { inherit inputs; };
-        #   modules = [
-        #     ./nixos-wsl/configuration.nix
-        #     nixos-wsl.nixosModules.default
-        #     {
-        #       system.stateVersion = "25.11";
-        #       wsl.enable = true;
-        #     }
-        #     home-manager.nixosModules.home-manager
-        #     {
-        #       home-manager.useGlobalPkgs = true;
-        #       home-manager.useUserPackages = true;
-        #       home-manager.extraSpecialArgs = { inherit inputs; };
-        #       home-manager.users.nixos = ./nixos-wsl/home.nix;
-        #     }
-        #   ];
-        # };
 
         nixos-qemu = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
